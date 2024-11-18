9ced8d3b8453278be3d2513051695ac57730c0f0
Tom <tomor.enkh@gmail.com>
2024-11-18 16:28:25 +0800

    implement dynamic burger ingredient handling based on data

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -3,14 +3,28 @@ import { BurgerIngredient } from "../BurgerIngredient";
 
 import css from "./style.module.css";
 
-export const Burger = () => {
+export const Burger = (props) => {
+  let content = [];
+
+  const items = Object.entries(props.ingredients);
+
+  items.map((el) => {
+    for (let i = 0; i < el[1]; i++)
+      content.push(<BurgerIngredient key={`${el[0]}${i + 1}`} type={el[0]} />);
+  });
+
+  if (content.length === 0) {
+    content = <p>Хачиртай талхныхаа орцыг сонгоно уу</p>;
+  }
+
   return (
     <div className={css.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="salad" />
+      {content}
+      {/* <BurgerIngredient type="salad" />
       <BurgerIngredient type="meat" />
       <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="bacon" />
+      <BurgerIngredient type="bacon" /> */}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/pages/BurgerBuilder/index.js
+++ src/pages/BurgerBuilder/index.js
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 import { Burger } from "../../components/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      meat: 0,
+      cheese: 0,
+      bacon: 0,
+    },
+  };
+
   render() {
     return (
       <div>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Орц удирдах хэсэг</div>
       </div>
     );
