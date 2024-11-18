c41cd22d874020118c2e0ea40c0da820115b72e2
Tom <tomor.enkh@gmail.com>
2024-11-18 19:16:17 +0800

    create and visualize BuildControl, BuildControls components

--- src/pages/BurgerBuilder/index.js
+++ src/pages/BurgerBuilder/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Burger } from "../../components/Burger";
+import { BuildControls } from "../../components/BuildControls";
 
 class BurgerBuilder extends Component {
   state = {
@@ -15,7 +16,7 @@ class BurgerBuilder extends Component {
     return (
       <div>
         <Burger ingredients={this.state.ingredients} />
-        <div>Орц удирдах хэсэг</div>
+        <BuildControls />
       </div>
     );
   }
